dcf90d8a41650b466a6d4ceb0821f1cad32ecc0f
Carlos <hola@carloselias.cl>
2026-07-06 16:46:58 -0400

    test dist

--- dist/style/sheet.d.ts
+++ dist/style/sheet.d.ts
@@ -1,2 +1,26 @@
-declare function luzStyleSheet(config?: any): void;
+/**
+* Full configuration for the `luz()` function.
+*/
+interface LuzConfig {
+	font?: string;
+	"line-height"?: string;
+	"font-bold-weight"?: number;
+	"font-weight"?: number;
+	"font-monospace"?: string;
+	"font-headings"?: string;
+	"font-emphasis"?: string;
+	base?: number;
+	power?: number;
+	primary: string;
+	name?: string;
+	secondary?: string;
+	mode?: "light" | "dark";
+	neutrals?: string;
+	prefix?: string;
+	transition?: string;
+	"box-shadow"?: string;
+	spacing?: string;
+	path?: string;
+}
+declare function luzStyleSheet(config?: LuzConfig): void;
 export { luzStyleSheet };
